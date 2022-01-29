f0bb42471a228262f3e5f98e60bfb2cdb4350a96
sancheznot <sancheznotp@gmail.com>
2022-01-28 19:24:00 -0800

    Registro de usuario completo

--- src/models/Users.js
+++ src/models/Users.js
@@ -1,14 +1,15 @@
 const {Schema, model} = require('mongoose');
 const cryptPW = require('bcryptjs');
 
-new Schema({
+const UserSchema = new Schema({
     name: {
         type: 'string',
         required: true
     },
     email:{
         type: 'string',
-        required: true
+        required: true,
+        unique: true
     },
     password:{
         type: 'string',
@@ -18,12 +19,12 @@ new Schema({
     timestamps: true
 });
 // Aqui se hace el cifrado de la contrase;a
-UserSchema.methods.encrypters = async password => {
+UserSchema.methods.encryptPassword = async password => {
    const salt = await cryptPW.genSalt(10);
    return await cryptPW.hash(password, salt);
 };
 // Aqui se hace comparacion con la contrase;a guardada en la base de datos, devuelve si es true o false
-UserSchema.methods.matchPassword = function(password){
+UserSchema.methods.matchPassword = async function(password){
     return await cryptPW.compare(password, this.password);
 };
 
